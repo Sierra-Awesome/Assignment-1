07bf405c20ffbd98b9ebceb78b09584b294fd576
Sierra-Awesome <57376954+Sierra-Awesome@users.noreply.github.com>
2019-12-03 17:12:17 -0500

    Add files via upload

---
+++ Variables & Conditionals Code Assignment 1.py
@@ -0,0 +1,42 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+echo "# Assignment-1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Sierra-Awesome/Assignment-1.git
+git push -u origin master
+>>> x = 5
+>>> y = "Sierra"
+>>> print(x)
+5
+>>> print(y)
+Sierra
+>>> 1 = "icecream"
+SyntaxError: cannot assign to literal
+>>> a = "icecream"
+>>> b = "hotdog"
+>>> c = "noodles"
+>>> print(a)
+icecream
+>>> print (c)
+noodles
+>>> print(b)
+hotdog
+>>> timeofday = input("10:04 (M,A,E): ")
+10:04 (M,A,E): 
+>>> print("icecream "+str(score)+" of the Core 4")
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    print("icecream "+str(score)+" of the Core 4")
+NameError: name 'score' is not defined
+>>>   print("hotdog "+str(a)+" noodles")
+  
+SyntaxError: unexpected indent
+>>> 
+KeyboardInterrupt
+>>> vprint("hotdog "+str(a)+" noodles")print("hotdog "+str(a)+" noodles")
+SyntaxError: invalid syntax
+>>> print("hotdog "+str(a)+" noodles")
+hotdog icecream noodles
+>>> 
